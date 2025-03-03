c9c826e23e9e92028ac9699fc3140cb14c50fb8b
tubar02 <toszerodois@gmail.com>
2025-03-03 19:52:29 -0300

    Começo da estruturação da função menu() dentro de main.py

--- Trabalho/Problemas/Problema 1/main.py
+++ Trabalho/Problemas/Problema 1/main.py
@@ -1,8 +1,24 @@
+import useful_lib
+
+#inicialização dos dados
+n_pontos = 2 ** 11 
+with open("first_parameters.txt", "r") as valores_iniciais: #recebe os primeiros dados
+	S_0 = valores_iniciais.readline().split()[2]
+	omega = valores_iniciais.readline().split()[2]
+	T = valores_iniciais.readline().split()[2]
+
 def menu():
-	pass
+	print("Bem-vindo à resolução do Problema 1.") 
+	print("Escolha sua opção: ")
+	print("\tS: Mostra o sinal gerado.")
+
+	escolha = str(input()).capitalize()
+
+	print(escolha)
 
 def main():
 	menu()
 	return 0
 
-main()
+if __name__ == "__main__":
+	main()
