4075c953fa265dc676710a0ada021ed71cd93e89
tubar02 <toszerodois@gmail.com>
2025-03-04 18:28:45 -0300

    Muda o nome da função calcula_sinal() para simula_sinal() e a implementa, junto com as funções cria_array_tempos e mostra_sinal(), em useful_lib.py

--- Trabalho/Problemas/Problema 1/useful_lib.py
+++ Trabalho/Problemas/Problema 1/useful_lib.py
@@ -5,25 +5,41 @@ import matplotlib.pyplot as plt
 n_pontos = 2 ** 11 
 dt = 5 * 10 ** (-4)
 with open("first_parameters.txt", "r") as valores_iniciais: #recebe os primeiros dados
-	S_0 = valores_iniciais.readline().split()[2]
-	omega = valores_iniciais.readline().split()[2]
-	T = valores_iniciais.readline().split()[2]
+	S_0 = float(valores_iniciais.readline().split()[2])
+	omega = float(valores_iniciais.readline().split()[2])
+	T = float(valores_iniciais.readline().split()[2])
 
-def calcula_sinal(Dt = dt, omega_0 = omega, T_0 = T, n_Pontos = n_pontos):
+def cria_array_tempos(Dt = dt, n_Pontos = n_pontos):
 	array_tempos = np.linspace(0, (n_Pontos - 1) * Dt, n_Pontos) #cria o vetor com os tempos igualmente espaçados
-	array_sinal = np.exp(-array_tempos/T) * np.exp(1j * omega * array_tempos)
+	return array_tempos
 
-def mostra_sinal(sinal):
+def simula_sinal(array_tempos, omega_0 = omega, T_0 = T):
+	sinal = np.exp(-array_tempos/T_0) * np.exp(1j * omega_0 * array_tempos)
+	return sinal
 
-	return 0
+def mostra_sinal(tempo, sinal, separa_componentes = True):
+	if separa_componentes:
+		plt.plot(tempo, sinal.real, label="Parte real")
+		plt.plot(tempo, sinal.imag, label="Parte imaginária")
+	else:
+		plt.plot(tempo, np.abs(sinal), label="Módulo")
+		
+	plt.xlabel("Tempo (s)")
+	plt.ylabel("Sinal(t)")
+	plt.title("Sinal Simulado")
+	plt.legend()
+	plt.grid()
+	plt.show()
 
 def aplica_FFT_em_sinal(sinal):
 	return 0
 
 def main():
-	array, step = np.linspace(0, 2047 * 0.5, 2048, retstep=True)
-	print(array)
-	print("Espaçamento entre os valores:", step)
+	tempo = cria_array_tempos(0.0005, 2048)
+	sinal = simula_sinal(tempo, 50)
+
+	mostra_sinal(tempo, sinal)
+	mostra_sinal(tempo, sinal, False)
 
 	return 0
 
