bb60ac172794911c6b0240c7221ed32a1d05a30a
tubar02 <toszerodois@gmail.com>
2025-03-04 17:13:25 -0300

    Define as funções mostra_sinal() e apica_FFT_em_sinal() no arquivo useful_lib.py, mas não as implementa

--- Trabalho/Problemas/Problema 1/useful_lib.py
+++ Trabalho/Problemas/Problema 1/useful_lib.py
@@ -1,7 +1,23 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+def mostra_sinal(sinal):
+
+	return 0
+
+def aplica_FFT_em_sinal(sinal):
+	return 0
 
 def main():
-	print("aaa")
+	t = np.array([0, 1, 2, 3])  
+	T = 2
+
+	resultado = np.exp(-t/T)  # Aplica e^(-t/T) a cada elemento
+	print(resultado)
+
+	for k in resultado:
+		print(k)
+
 	return 0
 
 if __name__ == "__main__":
