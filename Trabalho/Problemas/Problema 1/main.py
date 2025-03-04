b4f98507cb46e0b55c50424e5b01cdd8baca2473
tubar02 <toszerodois@gmail.com>
2025-03-04 17:29:41 -0300

    Passa a inicialização dos dados da main.py para a useful_lib.py, além de começar a implementação da função calcula_sinal() na useful_lib.py

--- Trabalho/Problemas/Problema 1/main.py
+++ Trabalho/Problemas/Problema 1/main.py
@@ -2,10 +2,15 @@ import useful_lib
 
 #inicialização dos dados
 n_pontos = 2 ** 11 
+dt = 5 * 10 ** (-4)
 with open("first_parameters.txt", "r") as valores_iniciais: #recebe os primeiros dados
 	S_0 = valores_iniciais.readline().split()[2]
 	omega = valores_iniciais.readline().split()[2]
 	T = valores_iniciais.readline().split()[2]
+
+def calcula_sinal(Dt = dt, omega_0 = omega, T_0 = T, n_Pontos = n_pontos):
+	array_tempos = np.linspace(0, (n_Pontos - 1) * Dt, n_Pontos) #cria o vetor com os tempos igualmente espaçados
+	array_sinal = np.exp(-array_tempos/T) * np.exp(1j * omega * array_tempos)
 
 def menu():
 	print("Bem-vindo à resolução do Problema 1.") 
--- Trabalho/Problemas/Problema 1/useful_lib.py
+++ Trabalho/Problemas/Problema 1/useful_lib.py
@@ -1,6 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+#inicialização dos dados
+n_pontos = 2 ** 11 
+dt = 5 * 10 ** (-4)
+with open("first_parameters.txt", "r") as valores_iniciais: #recebe os primeiros dados
+	S_0 = valores_iniciais.readline().split()[2]
+	omega = valores_iniciais.readline().split()[2]
+	T = valores_iniciais.readline().split()[2]
+
+def calcula_sinal(Dt = dt, omega_0 = omega, T_0 = T, n_Pontos = n_pontos):
+	array_tempos = np.linspace(0, (n_Pontos - 1) * Dt, n_Pontos) #cria o vetor com os tempos igualmente espaçados
+	array_sinal = np.exp(-array_tempos/T) * np.exp(1j * omega * array_tempos)
+
 def mostra_sinal(sinal):
 
 	return 0
@@ -9,14 +21,9 @@ def aplica_FFT_em_sinal(sinal):
 	return 0
 
 def main():
-	t = np.array([0, 1, 2, 3])  
-	T = 2
-
-	resultado = np.exp(-t/T)  # Aplica e^(-t/T) a cada elemento
-	print(resultado)
-
-	for k in resultado:
-		print(k)
+	array, step = np.linspace(0, 2047 * 0.5, 2048, retstep=True)
+	print(array)
+	print("Espaçamento entre os valores:", step)
 
 	return 0
 
